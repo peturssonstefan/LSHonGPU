ae08acff82ace8ac6a7bcb427ac5ab21082cf76e
sbjr <sbjr@itu.dk>
2019-02-15 09:08:03 +0100

    kernel now does knn for test data set

--- LinearScan/kernel.cu
+++ LinearScan/kernel.cu
@@ -5,15 +5,17 @@
 #include<iostream>
 #include "gloveparser.cuh"
 #include <stdio.h>
+#include <thrust/sort.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
 __global__
-void add(int n, int d, float *x, float *y, float *z) {
-	 
+void add(int n, int d, int k, float *x, float *y, float *z) {
 
 	float dotProduct; 
 	int queryIndex = threadIdx.x;
 	int index = queryIndex * d;
-
+	int z_index = queryIndex * k; 
 	for (int i = 0; i < n; i++) {
 		float dotProduct = 0; 
 		float magnitude_x = 0.0;
@@ -26,9 +28,15 @@ void add(int n, int d, float *x, float *y, float *z) {
 
 		magnitude_x = sqrt(magnitude_x);
 		magnitude_y = sqrt(magnitude_y);
-		z[queryIndex * n + i] = -(dotProduct / (magnitude_x * magnitude_y)); 
-		if (i == 0) {
-			printf("Q %d = %f \n",queryIndex, z[queryIndex*n]); 
+		float angular_distance = -(dotProduct / (magnitude_x * magnitude_y));
+		//z[z_index + i] = angular_distance; 
+		float tmp_distance = 0; 
+		for (int j = 0; (j < k && j < i); j++) { // simple sorting.
+			if (z[z_index + j] > angular_distance) {
+				tmp_distance = z[z_index + j]; 
+				z[z_index + j] = angular_distance; 
+				angular_distance = tmp_distance; 
+			}
 		}
 	}
 }
@@ -38,10 +46,8 @@ int main(int argc, char **argv)
 {
 	char* filepath_data = argv[1];
 	char* filepath_queries = argv[2];
-	//char* dim = argv[3];
-	//char* _k = argv[3];
-	//int dimensions = atoi(dim);
-	//int k = atoi(_k);
+	char* _k = argv[3];
+	int k = atoi(_k);
 
 	int N_data = 0;
 	int N_query = 0; 
@@ -59,29 +65,30 @@ int main(int argc, char **argv)
 	x = parseFile(filepath_queries, N_query, d); 
 	y = parseFile(filepath_data, N_data, d);
 	printf("Done parsing files. \n");
-	z = (float*)malloc(N_data*N_query * sizeof(float)); 
-
-	/*for (int i = 0; i < N_query * d; i++) {
-		x[i] = 1.0f;
+	printf("N_Query = %d \n", N_query);
+	printf("N_Data = %d \n", N_data); 
+	printf("k is set to: %d\n", k);
+	z = (float*)malloc(k*N_query * sizeof(float));
+	
+	for (int i = 0; i < k * N_query; i++) {
+		z[i] = 2.0f; //fill z array with default max value. 
 	}
-
-	for (int i = 0; i < N_data*d; i++) {
-		y[i] = 2.0f;
-	}*/
 
 	float* dev_x = 0;
 	float* dev_y = 0;
 	float* dev_z = 0;
 	cudaMalloc((void**)&dev_x, N_query * d * sizeof(float));
 	cudaMalloc((void**)&dev_y, N_data * d * sizeof(float));
-	cudaMalloc((void**)&dev_z, N_data * N_query * sizeof(float));
+	cudaMalloc((void**)&dev_z, k * N_query * sizeof(float));
 
 	cudaMemcpy(dev_x, x, N_query * d * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_y, y, N_data * d * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_z, y, N_data * N_query * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_z, z, k * N_query * sizeof(float), cudaMemcpyHostToDevice);
 	// initialize x and y arrays on the host
 
-	add << <1, N_query>> > (N_data, d, dev_x, dev_y, dev_z);
+	add << <1, N_query>> > (N_data, d, k, dev_x, dev_y, dev_z);
+
+
 
 	cudaStatus = cudaGetLastError();
 	if (cudaStatus != cudaSuccess) {
@@ -95,16 +102,16 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	cudaStatus = cudaMemcpy(z, dev_z, N_data * N_query * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaStatus = cudaMemcpy(z, dev_z, k * N_query * sizeof(float), cudaMemcpyDeviceToHost);
 
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		fprintf(stderr, "cuda memcpy from device to host returned error code %d \n", cudaStatus);
 		return -1;
 	}
 
-	//for (int i = 0; i < N_query*d; i++) { 
-	//	printf
-	//}
+	for (int i = 0; i < k*N_query; i++) { 
+		printf("z[%d] = %f\n", i, z[i]); 
+	}
 
 	cudaFree(dev_x);
 	cudaFree(dev_y);
