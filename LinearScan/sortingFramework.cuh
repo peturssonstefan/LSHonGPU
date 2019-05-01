0473233897214bdffb3fb312ad8e1b0b4c9ad87f
sbjr <sbjr@itu.dk>
2019-05-01 13:53:41 +0200

    changed increment to int

--- LinearScan/sortingFramework.cuh
+++ LinearScan/sortingFramework.cuh
@@ -176,10 +176,11 @@ void laneStrideSort(Point* val, Point swapPoint, Parameters& params) {
 
 
 			params.start = params.lane % 2 == 0 ? pairCouple * params.elemsToExchange : pairCouple * params.elemsToExchange + params.elemsToExchange - 1;
-			params.increment = params.lane % 2 == 0 ? 1 : -1;
+			//params.increment = params.lane % 2 == 0 ? 1 : -1;
 			params.end = params.elemsToExchange + (pairCouple * params.elemsToExchange);
 			int smallestLoopVal = pairCouple * params.elemsToExchange; 
-			for (int i = params.start; i < params.end && i >= smallestLoopVal; i += params.increment) {
+			int increment = params.lane % 2 == 0 ? 1 : -1; 
+			for (int i = params.start; i < params.end && i >= smallestLoopVal; i += increment) {
 
 			}
 			//for (int i = params.start; i < params.end && i >= pairCouple * params.elemsToExchange; i += params.increment) {
