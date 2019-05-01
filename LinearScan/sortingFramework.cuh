d1f663c4cd3f02865526823f28058f899da9db1c
sbjr <sbjr@itu.dk>
2019-05-01 13:31:23 +0200

    debugging.

--- LinearScan/sortingFramework.cuh
+++ LinearScan/sortingFramework.cuh
@@ -174,8 +174,8 @@ void laneStrideSort(Point* val, Point swapPoint, Parameters& params) {
 
 		for (int pairCouple = 0; pairCouple < (threadQueueSize / 2); pairCouple++) {
 
-			//params.start = params.lane % 2 == 0 ? pairCouple * params.elemsToExchange : pairCouple * params.elemsToExchange + params.elemsToExchange - 1;
-			//params.increment = params.lane % 2 == 0 ? 1 : -1;
+			params.start = params.lane % 2 == 0 ? pairCouple * params.elemsToExchange : pairCouple * params.elemsToExchange + params.elemsToExchange - 1;
+			params.increment = params.lane % 2 == 0 ? 1 : -1;
 			//params.end = params.elemsToExchange + (pairCouple * params.elemsToExchange);
 			//for (int i = params.start; i < params.end && i >= pairCouple * params.elemsToExchange; i += params.increment) {
 			//	params.allIdx = params.lane + warpSize * i;
