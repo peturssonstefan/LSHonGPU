ce09263546bcc6a6644e67954cc779c0001eb1cb
sbjr <sbjr@itu.dk>
2019-05-01 13:42:47 +0200

    added dummy loop

--- LinearScan/sortingFramework.cuh
+++ LinearScan/sortingFramework.cuh
@@ -178,6 +178,10 @@ void laneStrideSort(Point* val, Point swapPoint, Parameters& params) {
 			params.start = params.lane % 2 == 0 ? pairCouple * params.elemsToExchange : pairCouple * params.elemsToExchange + params.elemsToExchange - 1;
 			params.increment = params.lane % 2 == 0 ? 1 : -1;
 			params.end = params.elemsToExchange + (pairCouple * params.elemsToExchange);
+
+			for (int i = 0; i < 10; i++) {
+
+			}
 			//for (int i = params.start; i < params.end && i >= pairCouple * params.elemsToExchange; i += params.increment) {
 			//	params.allIdx = params.lane + warpSize * i;
 			//	params.pairIdx = params.allIdx / pairSize;
