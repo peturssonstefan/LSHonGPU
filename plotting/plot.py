0b81d42573f95583687cd37eaccb36df55181a2f
Fredrik Bakke <femb@itu.dk>
2019-05-07 14:41:50 +0200

    plotting for lsh algorithms

--- plotting/plot.py
+++ plotting/plot.py
@@ -7,22 +7,25 @@ import numpy as np
 
 matplotlib.style.use("seaborn")
 
-fileName = sys.argv[1]
-dataset = sys.argv[2]
-runPlots = int(sys.argv[3])
-numQueries = int(sys.argv[4])
+sketchFileName = sys.argv[1]
+lshFileName = sys.argv[2]
+dataset = sys.argv[3]
+runPlots = int(sys.argv[4])
+numQueries = int(sys.argv[5])
 
 kValues = [32,64,128,256,512,1024]
 tqSizes = [2,4,8,16,32,64,128]
 
-print(f"Reading file: {fileName}")
-namesArr = ["implementation","keyImplementation","sketchDim","k","THREAD_QUEUE_SIZE","bucketKeyBits","tables","WITH_TQ_OR_BUFFER","preprocessTime","constructionTime","scanTime","recall","avgDistance"]
-data = np.genfromtxt(fileName, dtype=None, names=namesArr, delimiter=",")
-data = data[data["scanTime"].argsort()]
-# print(data)
+def readData(fileName):
+    print(f"Reading file: {fileName}")
+    namesArr = ["implementation","keyImplementation","sketchDim","k","THREAD_QUEUE_SIZE","bucketKeyBits","tables","WITH_TQ_OR_BUFFER","preprocessTime","constructionTime","scanTime","recall","avgDistance"]
+    data = np.genfromtxt(fileName, dtype=None, names=namesArr, delimiter=",")
+    data = data[data["scanTime"].argsort()]
+    # print(data)
 
-# transform data scantime to queries per second
-data["scanTime"] = numQueries/(data["scanTime"]/1000) 
+    # transform data scantime to queries per second
+    data["scanTime"] = numQueries/(data["scanTime"]/1000)     
+    return data
 
 yMajorTicks = np.arange(0,1.1,0.2)
 
@@ -81,7 +84,39 @@ def plotDistanceRatioVsScanTime(ax, dataK, k, tqSize):
     ax.legend()
 
 
-def runPlotRecallVsTimeData():     
+def buildLshSeries(ax, data, implementation, bucketImplementation, x, y, label):
+    mask = (data["implementation"]==implementation) & (data["keyImplementation"]==bucketImplementation)
+    ax.plot(data[mask][x],data[mask][y],'o', label=label)
+
+def plotLshAlgorithmsRecallVsScanTime(ax, dataK, k, tqSize):
+    buildLshSeries(ax, dataK, 2, 3, "recall", "scanTime", "SIMHASH - scan")
+    buildLshSeries(ax, dataK, 3, 3, "recall", "scanTime", "SIMHASH - SIMHASH")
+    buildLshSeries(ax, dataK, 5, 3, "recall", "scanTime", "SIMHASH - 1BITMINHASH")
+
+    buildLshSeries(ax, dataK, 2, 5, "recall", "scanTime", "1BITMINHASH - scan")
+    buildLshSeries(ax, dataK, 3, 5, "recall", "scanTime", "1BITMINHASH - SIMHASH")
+    buildLshSeries(ax, dataK, 5, 5, "recall", "scanTime", "1BITMINHASH - 1BitMINHASH")    
+
+    buildLshSeries(ax, dataK, 2, 7, "recall", "scanTime", "CROSSPOLY - scan")
+    buildLshSeries(ax, dataK, 3, 7, "recall", "scanTime", "CROSSPOLY - SIMHASH")
+    buildLshSeries(ax, dataK, 5, 7, "recall", "scanTime", "CROSSPOLY - 1BitMINHASH") 
+
+    ax.set_xlabel("Recall")
+    ax.set_ylabel("Queries per second")
+    ax.set_title(f"{dataset} LSH results k={k} ThreadQueueSize={tqSize}")
+    #ax.set_yticks(yMajorTicks)
+    ax.legend()
+
+def runPlotLshRecallVsTime(data, lshData):
+    for tqSize in tqSizes:
+        dataTQ = lshData[lshData["THREAD_QUEUE_SIZE"]==tqSize]
+        for currentK in kValues:
+            fig , axes = plt.subplots(nrows=1, ncols=1, constrained_layout=True)
+            plotLshAlgorithmsRecallVsScanTime(axes, dataTQ[dataTQ["k"]==currentK], currentK, tqSize)
+            plt.savefig(f"plots/{dataset}lshrecallvstime{currentK}k_{tqSize}tq.png")
+            plt.close(fig)
+
+def runPlotRecallVsTimeData(data, lshData):     
     for tqSize in tqSizes:
         dataTQ = data[data["THREAD_QUEUE_SIZE"]==tqSize]
         for currentK in kValues:
@@ -91,7 +126,7 @@ def runPlotRecallVsTimeData():
             plt.close(fig)
 
 
-def runPlotDistanceRatioVsTime():
+def runPlotDistanceRatioVsTime(data, lshData):
     for tqSize in tqSizes:
         dataTQ = data[data["THREAD_QUEUE_SIZE"]==tqSize]
         for currentK in kValues:
@@ -100,7 +135,7 @@ def runPlotDistanceRatioVsTime():
             plt.savefig(f"plots/{dataset}distancevstime{currentK}k_{tqSize}tq.png")
             plt.close(fig)
 
-def runPlotRecallVsBits():
+def runPlotRecallVsBits(data, lshData):
     dataSort = data[data["sketchDim"].argsort()]
     
     for tqSize in tqSizes:
@@ -111,7 +146,7 @@ def runPlotRecallVsBits():
             plt.savefig(f"plots/{dataset}recallvsbits{currentK}k_{tqSize}tq.png")
             plt.close(fig)
 
-def runPlotBufVsSort():
+def runPlotBufVsSort(data, lshData):
     dataSortTQ = data[data["THREAD_QUEUE_SIZE"].argsort()]
     dataMemOptimized = dataSortTQ[dataSortTQ["implementation"]==2]
     dataBuffer = dataMemOptimized[dataMemOptimized["WITH_TQ_OR_BUFFER"]==0]
@@ -121,25 +156,32 @@ def runPlotBufVsSort():
         fig , axes = plt.subplots(nrows=1, ncols=1, constrained_layout=True)
         plotBufferVsSort(axes, dataSort[dataSort["k"]==currentK], dataBuffer[dataBuffer["k"]==currentK], currentK)
         plt.savefig(f"plots/{dataset}bufvssort{currentK}k.png")
+        plt.close(fig)
 
 
-def runAll():
-    runPlotBufVsSort()
-    runPlotRecallVsTimeData()
-    runPlotRecallVsBits()
-    runPlotDistanceRatioVsTime()
+def runAll(data, lshData):
+    runPlotBufVsSort(data, lshData)
+    runPlotRecallVsTimeData(data, lshData)
+    runPlotRecallVsBits(data, lshData)
+    runPlotDistanceRatioVsTime(data, lshData)
+    runPlotLshRecallVsTime(data, lshData)
 
 
 # Main 
+
+sketchedData = readData(sketchFileName)
+lshDataFile = readData(lshFileName)
+
 
 switcher = {
     0: runPlotBufVsSort,
     1: runPlotRecallVsTimeData,
     2: runPlotRecallVsBits,
-    3: runPlotDistanceRatioVsTime
+    3: runPlotDistanceRatioVsTime,
+    4: runPlotLshRecallVsTime
 }
 
 print(f"Running {runPlots}")
 runner = switcher.get(runPlots, runAll)
 
-runner()
+runner(sketchedData, lshDataFile)
